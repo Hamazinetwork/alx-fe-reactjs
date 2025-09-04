0f2da548ba1ecc5dca37ca4291e98f9e937a1335
hamazin <hademolahamazin@gmail.com>
2025-09-04 14:46:04 +0100

    styling components

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,22 @@
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile' 
+import Counter from './components/Counter'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter/> 
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main style={{margin: '10px'}}>
+        <p style={{textAlign: 'center', color: 'blue'}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  )
+}
+
+export default MainContent
