8ac54e0df97824a8c54d25e6e3a28eeb5838ccc7
hamazin <hademolahamazin@gmail.com>
2025-09-21 21:11:34 +0100

    search functionality

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Search from "./components/Search";
 
-const App = () => {
+function App() {
   return (
     <div>
-      
+      <h1 style={{ textAlign: "center", marginTop: "1rem" }}>
+        GitHub User Search
+      </h1>
+      <Search />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
